a4e96cbd5dc5fe821b384682b4bcb9f9008a4aad
Sheila-a <ugwuodo20@gmail.com>
2023-01-29 23:03:03 +0100

    Completed index file

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-import dialNumber from "./component/dialNumber";
+import State from "./js/state.js";
+import phoneNumbers from "./component/phoneNumbers.js";
+import printNumber from "./component/printNumber.js";
+import dialNumber from "./component/dialNumber.js";
+
+// This is the state of our Telephone package instantiated from the State class
+const TelephoneState = new State();
+
+// This is the telephone list object instantiated from the phoneNumbers class
+const UserPhoneNumbers = new phoneNumbers().addPhoneNumber(09034678110);
+
+// This is the object created from the printNumber class which prints the phone Number
+const printedNum = new printNumber();
 
 // This is the object created from the dialNumber class which prints the dialing message and number
 const dialedNum = new dialNumber();
+
+// Two observers added
+TelephoneState.addObserver(printedNum);
+TelephoneState.addObserver(dialedNum);
+
+// To perform initial render on load
+printedNum.render(TelephoneState.get());
+dialedNum.render(TelephoneState.get());
+
+console.log(UserPhoneNumbers);
