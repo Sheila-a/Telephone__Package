830b587155082cd139ca748990847acd5df2a8f0
Sheila-a <ugwuodo20@gmail.com>
2023-01-29 12:39:36 +0100

    Builder file

---
+++ index.js
@@ -0,0 +1,4 @@
+import dialNumber from "./component/dialNumber";
+
+// This is the object created from the dialNumber class which prints the dialing message and number
+const dialedNum = new dialNumber();
